ce290dbf01d0369dfcc837d1228b55f6503d729c
Ludovic Fauvet <etix@l0cal.com>
2009-03-28 11:55:26 +0100

    Swedish translation
    Thanks yeager!

---
+++ vlmc_sv.ts
@@ -0,0 +1,196 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sv" sourcelanguage="en">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>LibraryWidget</name>
+    <message>
+        <location filename="src/gui/LibraryWidget.cpp" line="99"/>
+        <source>Open Audios</source>
+        <translation>Öppna ljud</translation>
+    </message>
+    <message>
+        <location filename="src/gui/LibraryWidget.cpp" line="100"/>
+        <source>Audio Files</source>
+        <translation>Ljudfiler</translation>
+    </message>
+    <message>
+        <location filename="src/gui/LibraryWidget.cpp" line="105"/>
+        <source>Video Files</source>
+        <translation>Videofiler</translation>
+    </message>
+    <message>
+        <location filename="src/gui/LibraryWidget.cpp" line="110"/>
+        <source>Images Files</source>
+        <translation>Bildfiler</translation>
+    </message>
+    <message>
+        <location filename="src/gui/LibraryWidget.cpp" line="104"/>
+        <source>Open Videos</source>
+        <translation>Öppna video</translation>
+    </message>
+    <message>
+        <location filename="src/gui/LibraryWidget.cpp" line="109"/>
+        <source>Open Images</source>
+        <translation>Öppna bilder</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/LibraryWidget.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formulär</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/LibraryWidget.ui" line="33"/>
+        <source>Audio</source>
+        <translation>Ljud</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/LibraryWidget.ui" line="59"/>
+        <source>Videos</source>
+        <translation>Video</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/LibraryWidget.ui" line="85"/>
+        <source>Images</source>
+        <translation>Bilder</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/LibraryWidget.ui" line="114"/>
+        <source>Add Media</source>
+        <translation>Lägg till media</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/LibraryWidget.ui" line="130"/>
+        <source>Remove Media</source>
+        <translation>Ta bort media</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/gui/MainWindow.cpp" line="61"/>
+        <source>Media Library</source>
+        <translation>Mediabibliotek</translation>
+    </message>
+    <message>
+        <location filename="src/gui/MainWindow.cpp" line="67"/>
+        <source>Preview</source>
+        <translation>Förhandsvisa</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/MainWindow.ui" line="14"/>
+        <source>VideoLAN Movie Creator</source>
+        <translation>VideoLAN Filmskapare</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/MainWindow.ui" line="31"/>
+        <source>File</source>
+        <translation>Arkiv</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/MainWindow.ui" line="37"/>
+        <source>Edit</source>
+        <translation>Redigera</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/MainWindow.ui" line="43"/>
+        <source>View</source>
+        <translation>Visa</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/MainWindow.ui" line="48"/>
+        <source>Effects</source>
+        <translation>Effekter</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/MainWindow.ui" line="53"/>
+        <source>Tools</source>
+        <translation>Verktyg</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/MainWindow.ui" line="58"/>
+        <source>Window</source>
+        <translation>Fönster</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/MainWindow.ui" line="63"/>
+        <location filename="src/gui/ui/MainWindow.ui" line="90"/>
+        <source>Help</source>
+        <translation>Hjälp</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/MainWindow.ui" line="80"/>
+        <source>Quit</source>
+        <translation>Avsluta</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/MainWindow.ui" line="85"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/MainWindow.ui" line="95"/>
+        <source>Preferences</source>
+        <translation>Inställningar</translation>
+    </message>
+</context>
+<context>
+    <name>Preferences</name>
+    <message>
+        <location filename="src/gui/ui/Preferences.ui" line="17"/>
+        <source>Preferences</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/Preferences.ui" line="30"/>
+        <source>General</source>
+        <translation>Allmänt</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/Preferences.ui" line="42"/>
+        <source>Language</source>
+        <translation>Språk</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/Preferences.ui" line="74"/>
+        <source>Apply</source>
+        <translation>Verkställ</translation>
+    </message>
+    <message>
+        <location filename="src/gui/ui/Preferences.ui" line="81"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="src/gui/Preferences.cpp" line="9"/>
+        <source>English</source>
+        <translation>Engelska</translation>
+    </message>
+    <message>
+        <location filename="src/gui/Preferences.cpp" line="10"/>
+        <source>French</source>
+        <translation>Franska</translation>
+    </message>
+    <message>
+        <location filename="src/gui/Preferences.cpp" line="11"/>
+        <source>Spanish</source>
+        <translation>Spanska</translation>
+    </message>
+</context>
+<context>
+    <name>PreviewWidget</name>
+    <message>
+        <location filename="src/gui/ui/PreviewWidget.ui" line="17"/>
+        <source>Dialog</source>
+        <translation>Dialogruta</translation>
+    </message>
+</context>
+<context>
+    <name>Timeline</name>
+    <message>
+        <location filename="src/gui/ui/Timeline.ui" line="20"/>
+        <source>Form</source>
+        <translation>Formulär</translation>
+    </message>
+</context>
+</TS>
