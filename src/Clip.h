1192c4324abe850e4f8956815741574f3f3da4f3
Hugo Beauzee-Luyssen <beauze.h@gmail.com>
2009-04-24 13:59:46 +0200

    Now you can build the project :o)

---
+++ src/Clip.h
@@ -0,0 +1,45 @@
+/*****************************************************************************
+ * Media.h : Represents a basic container for media informations.
+ *****************************************************************************
+ * Copyright (C) 2008-2009 the VLMC team
+ *
+ * Authors: Hugo Beauzee-Luyssen <hugo@vlmc.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+/** \file
+  * This file contains the Clip class declaration/definition.
+  * It's used by the timeline in order to represent a subset of a media
+  */
+
+#ifndef CLIP_H__
+# define CLIP_H__
+
+#include <QObject>
+
+#include "Media.h"
+
+class   Clip : public QObject
+{
+    Q_OBJECT
+
+    public:
+        Clip( Media* parent );
+    private:
+
+};
+
+#endif //CLIP_H__
