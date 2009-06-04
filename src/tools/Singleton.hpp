d86381bd714c20649185d3f14f38fa6cfd38dfa2
Hugo Beauzee-Luyssen <beauze.h@gmail.com>
2009-06-04 07:19:16 +0200

    Added forgotten GPL header, corrected english :)

--- src/tools/Singleton.hpp
+++ src/tools/Singleton.hpp
@@ -1,9 +1,32 @@
+/*****************************************************************************
+ * Singleton.hpp : Generic singleton pattern implementation
+ *****************************************************************************
+ * Copyright (C) 2008-2009 the VLMC team
+ *
+ * Authors: Hugo Beauzee-Luyssen <hugo@vlmc.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+
 /** \file
   * This file contain the templated singleton.
   * Class/struct to be singletonized just have to
   * iherit from this classe with the class/struct type in template
   * parameter.You have to do few other things, but you know your job,
-  * isnt'it :) ?
+  * don't you :) ?
   */
 
 #ifndef SINGLETON_HPP
