2f408c6617fc73dc9fe3596739436f73c726ec56
Hugo Beauzee-Luyssen <beauze.h@gmail.com>
2009-07-06 19:15:11 +0200

    Removed debug, updated header

--- src/renderer/ClipRenderer.cpp
+++ src/renderer/ClipRenderer.cpp
@@ -4,6 +4,7 @@
  * Copyright (C) 2008-2009 the VLMC team
  *
  * Authors: Geoffroy Lacarrière <geoffroylaca@gmail.com>
+ *          Hugo Beauzee-Luyssen <hugo@vlmc.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
