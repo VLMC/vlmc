b002ab6c4f700cdfb40946ffb87a3c0b2f97bec7
Vincent Carrubba <cyberbouba@gmail.com>
2009-07-30 16:42:30 +0200

    The same things as before, but with license and my right mail -_-

--- effects_engine/OutSlot.hpp
+++ effects_engine/OutSlot.hpp
@@ -1,3 +1,26 @@
+/*****************************************************************************
+ * OutSlot.hpp: OutSlot is for outing data to an InSlot for inter-effects
+ * communication
+ *****************************************************************************
+ * Copyright (C) 2008-2009 the VLMC team
+ *
+ * Authors: Vincent Carrubba <cyberbouba@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #ifndef OUTSLOT_HPP_
 #define OUTSLOT_HPP_
 
@@ -97,6 +120,7 @@ void	OutSlot<T>::resetPipe( void )
   m_pipe = &m_junk;
   return ;
 }
+
 void	OutSlot<T>::setInSlotPtr( InSlot<T>* ptr )
 {
   m_connectedto = ptr;
