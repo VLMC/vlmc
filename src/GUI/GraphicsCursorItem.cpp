VLMC/vlmc

commit e602d8e53b2e6c7192d517f66a69f5da087b8ae6

Remove unused variable

src/GUI/GraphicsCursorItem.cpp
@@ -1,7 +1,7 @@
 #include <QtDebug>
 #include "GraphicsCursorItem.h"
 
-GraphicsCursorItem::GraphicsCursorItem( int height, const QPen& pen )
+GraphicsCursorItem::GraphicsCursorItem( const QPen& pen )
         : m_pen( pen )
 {
     setFlags( QGraphicsItem::ItemIgnoresTransformations | QGraphicsItem::ItemIsMovable );

src/GUI/GraphicsCursorItem.h
@@ -13,7 +13,7 @@ class GraphicsCursorItem : public QObject, public QGraphicsItem
 {
     Q_OBJECT
 public:
-    GraphicsCursorItem( int height, const QPen& pen );
+    GraphicsCursorItem( const QPen& pen );
     int cursorPos() const { return ( int )pos().x(); }
     virtual QRectF boundingRect() const;
     void setHeight( int height );

src/GUI/TracksView.cpp
@@ -57,8 +57,7 @@ TracksView::TracksView( QGraphicsScene* scene, MainWorkflow* mainWorkflow, QWidg
     setContentsMargins( 0, 0, 0, 0 );
     setFrameStyle( QFrame::NoFrame );
 
-    //TODO Remove the GraphicsCursorItem parameter height (not useful anymore)
-    m_cursorLine = new GraphicsCursorItem( 1, QPen( QColor( 220, 30, 30 ) ) );
+    m_cursorLine = new GraphicsCursorItem( QPen( QColor( 220, 30, 30 ) ) );
 
     m_scene->addItem( m_cursorLine );
 

commit e18591737662320b5b95bf19aba444a4394e7f94

Fix build issue
Problem introduced in e602d8e53b2e6c7192d517f66a69f5da087b8ae6

src/GUI/GraphicsCursorItem.cpp
@@ -8,8 +8,7 @@ GraphicsCursorItem::GraphicsCursorItem( const QPen& pen )
     setCursor( QCursor( Qt::SizeHorCursor ) );
     setZValue( 100 );
 
-//    int height;
-    m_boundingRect = QRectF( -2, 0, 3, height );
+    m_boundingRect = QRectF( -2, 0, 3, 0 );
 }
 
 QRectF GraphicsCursorItem::boundingRect() const