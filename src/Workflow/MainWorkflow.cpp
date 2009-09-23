88fca5d13b2b0abc21bf2812e9e7a44aacf1313b
Hugo Beauzee-Luyssen <beauze.h@gmail.com>
2009-09-23 10:43:33 +0200

    Removed debug

--- src/Workflow/MainWorkflow.cpp
+++ src/Workflow/MainWorkflow.cpp
@@ -326,7 +326,7 @@ void        MainWorkflow::trackUnpaused()
 
 void        MainWorkflow::tracksRenderCompleted( unsigned int trackId )
 {
-    qDebug() << "tracksRenderCompleted";
+//    qDebug() << "tracksRenderCompleted";
     QMutexLocker    lockNbTracks( m_nbTracksToRenderMutex );
     --m_nbTracksToRender;
 
@@ -344,7 +344,7 @@ void        MainWorkflow::tracksRenderCompleted( unsigned int trackId )
     //therefore, m_nbTracksToRender will be equal to -1
     if ( m_nbTracksToRender <= 0 )
     {
-        qDebug() << "main workflow render completed";
+//        qDebug() << "main workflow render completed";
         //Just a synchronisation barriere
         {
             QMutexLocker    lock( m_synchroneRenderWaitConditionMutex );
@@ -357,9 +357,9 @@ unsigned char*  MainWorkflow::getSynchroneOutput()
 {
     m_synchroneRenderWaitConditionMutex->lock();
     getOutput();
-    qDebug() << "Waiting for sync output";
+//    qDebug() << "Waiting for sync output";
     m_synchroneRenderWaitCondition->wait( m_synchroneRenderWaitConditionMutex );
-    qDebug() << "Got it";
+//    qDebug() << "Got it";
     m_synchroneRenderWaitConditionMutex->unlock();
     if ( m_synchroneRenderingBuffer == NULL )
         return MainWorkflow::blackOutput;
