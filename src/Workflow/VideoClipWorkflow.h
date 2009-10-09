a50130fef83f559deba11ca962f1a467b27ee578
Hugo Beauzee-Luyssen <beauze.h@gmail.com>
2009-10-10 00:35:02 +0200

    Adding a VideoClipWorkflow class

---
+++ src/Workflow/VideoClipWorkflow.h
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ * VideoClipWorkflow.h : Clip workflow. Will extract a single frame from a VLCMedia
+ *****************************************************************************
+ * Copyright (C) 2008-2009 the VLMC team
+ *
+ * Authors: Hugo Beauzee-Luyssen <hugo@vlmc.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifndef VIDEOCLIPWORKFLOW_H
+#define VIDEOCLIPWORKFLOW_H
+
+#include "ClipWorkflow.h"
+#include "Clip.h"
+
+class   VideoClipWorkflow : public ClipWorkflow
+{
+    public:
+        VideoClipWorkflow( Clip* clip );
+        void*                   getLockCallback();
+        void*                   getUnlockCallback();
+
+    private:
+        static void             lock( VideoClipWorkflow* clipWorkflow, void** pp_ret, int size );
+        static void             unlock( VideoClipWorkflow* clipWorkflow, void* buffer, int width, int height, int bpp, int size );
+};
+
+#endif // VIDEOCLIPWORKFLOW_H
