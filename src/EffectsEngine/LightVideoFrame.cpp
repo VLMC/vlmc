42d7da95957a1c013e280f8adbd188af6fef651e
Vincent Carrubba <cyberbouba@gmail.com>
2009-10-16 18:29:20 +0200

    Forgot to add file before commit..

--- src/EffectsEngine/LightVideoFrame.cpp
+++ src/EffectsEngine/LightVideoFrame.cpp
@@ -1,3 +1,27 @@
+/*****************************************************************************
+ * LightVideoFrame.cpp: class used to contains raw video frame, with
+ * copy-on-write and references conter mecanism for performance and avoiding
+ * memory leak
+ *****************************************************************************
+ * Copyright (C) 2008-2009 the VLMC team
+ *
+ * Authors: Vincent Carrubba <cyberbouba@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #include "LightVideoFrame.h"
 
 VideoFrame::~VideoFrame()
