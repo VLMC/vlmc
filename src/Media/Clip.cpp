d5b2f69503bc49f8a9da52f76b6c10438bf5990c
Christophe Courtaut <christophe.courtaut@gmail.com>
2009-11-03 18:03:25 +0100

    Fixed Clip Splitting

--- src/Media/Clip.cpp
+++ src/Media/Clip.cpp
@@ -180,8 +180,8 @@ void                Clip::setEnd( qint64 end )
 Clip*               Clip::split( qint64 endFrame )
 {
     Q_ASSERT( endFrame != m_end );
+    endFrame += m_begin;
     Clip*   newClip = new Clip( this, endFrame, m_end );
-    qDebug() << "Created new clip. begin:" << newClip->m_begin << "end:" << newClip->m_end;
     m_end = endFrame;
     computeLength();
     return newClip;
