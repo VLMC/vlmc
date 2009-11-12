3a734e61138fa28cdcb1510ee16735d7fb11f1f0
Hugo Beauzee-Luyssen <chouquette@otterz.localdomain>
2009-11-12 04:31:50 +0100

    Removed forgotten debug.

--- src/Renderer/WorkflowRenderer.cpp
+++ src/Renderer/WorkflowRenderer.cpp
@@ -151,7 +151,6 @@ void        WorkflowRenderer::checkActions()
 
 void        WorkflowRenderer::startPreview()
 {
-    qDebug() << m_mainWorkflow->getLengthFrame();
     if ( m_mainWorkflow->getLengthFrame() <= 0 )
         return ;
     m_mediaPlayer->setMedia( m_media );
