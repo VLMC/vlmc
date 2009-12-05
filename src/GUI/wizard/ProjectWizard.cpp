7d65b89fd153a2e90cc8f4f9a142ac2b15e057d0
Ludovic Fauvet <etix@l0cal.com>
2009-12-05 18:30:23 +0100

    Add missing headers

--- src/GUI/wizard/ProjectWizard.cpp
+++ src/GUI/wizard/ProjectWizard.cpp
@@ -4,6 +4,7 @@
  * Copyright (C) 2008-2009 the VLMC team
  *
  * Authors: Clement CHAVANCE <kinder@vlmc.org>
+ *          Ludovic Fauvet <etix@l0cal.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
