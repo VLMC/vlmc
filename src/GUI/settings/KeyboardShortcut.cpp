8d5c29b9786c21184111eab80165623a56c8113c
Hugo Beauzee-Luyssen <beauze.h@gmail.com>
2009-12-08 01:21:09 +0100

    forgotten files

---
+++ src/GUI/settings/KeyboardShortcut.cpp
@@ -0,0 +1,44 @@
+/*****************************************************************************
+ * KeyboardShortcut.cpp: Keyboard shortcut preferences
+ *****************************************************************************
+ * Copyright (C) 2008-2009 the VLMC team
+ *
+ * Authors: Hugo Beauzee-Luyssen <hugo@vlmc.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#include "KeyboardShortcut.h"
+#include "KeyboardShortcutInput.h"
+
+KeyboardShortcut::KeyboardShortcut( QWidget* parent ) :
+        PreferenceWidget( parent )
+{
+    m_layout = new QFormLayout( this );
+    m_layout->addRow( tr( "Switch to cut mode" ), new KeyboardShortcutInput( this ) );
+    setLayout( m_layout );
+}
+
+KeyboardShortcut::~KeyboardShortcut()
+{
+}
+
+void        KeyboardShortcut::load()
+{
+}
+
+void        KeyboardShortcut::save()
+{
+}
---
+++ src/GUI/settings/KeyboardShortcut.h
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ * KeyboardShortcut.h: Keyboard shortcut preferences
+ *****************************************************************************
+ * Copyright (C) 2008-2009 the VLMC team
+ *
+ * Authors: Hugo Beauzee-Luyssen <hugo@vlmc.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifndef KEYBOARDSHORTCUT_H
+#define KEYBOARDSHORTCUT_H
+
+#include <QFormLayout>
+
+#include "PreferenceWidget.h"
+
+class   KeyboardShortcut : public PreferenceWidget
+{
+    public:
+        KeyboardShortcut( QWidget* parent = 0 );
+        virtual ~KeyboardShortcut();
+        void    load();
+        void    save();
+    private:
+        QFormLayout*            m_layout;
+};
+
+#endif // KEYBOARDSHORTCUT_H
