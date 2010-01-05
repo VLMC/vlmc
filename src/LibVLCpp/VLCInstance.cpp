458d58a55175e98d2282442c4a46a6068a3a3f85
Hugo Beauzee-Luyssen <beauze.h@gmail.com>
2010-01-05 02:07:11 +0100

    Removed useless includes

--- src/LibVLCpp/VLCInstance.cpp
+++ src/LibVLCpp/VLCInstance.cpp
@@ -20,9 +20,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
-#include <cassert>
 #include "VLCInstance.h"
-#include <QtDebug>
 
 using namespace LibVLCpp;
 
@@ -30,7 +28,7 @@ Instance::Instance()
 {
     char const *argv[] =
     {
-//        "-vvvvv",
+        "-vvvvv",
         "--no-skip-frames",
 //        "--intf", "dummy",
         "--text-renderer", "dummy",
