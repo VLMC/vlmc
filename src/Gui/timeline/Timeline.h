178ff5077fda78c3c365d6fb855e2e21a2442d26
Ludovic Fauvet <etix@l0cal.com>
2010-01-08 17:48:24 +0100

    Timeline documentation.

--- src/Gui/timeline/Timeline.h
+++ src/Gui/timeline/Timeline.h
@@ -40,18 +40,54 @@ class Timeline : public QWidget
 public:
     explicit Timeline( WorkflowRenderer* renderer, QWidget *parent = 0 );
     virtual ~Timeline();
+    /// Return a pointer to the TracksView instance.
     TracksView*         tracksView() { return m_tracksView; }
+    /// Return a pointer to the TracksScene instance.
     TracksScene*        tracksScene() { return m_tracksScene; }
+    /// Return a pointer to the TracksRuler instance.
     TracksRuler*        tracksRuler() { return m_tracksRuler; }
+    /// Return a pointer to the Timeline instance (singleton).
     static Timeline*    getInstance() { return m_instance; }
 
 public slots:
+    /**
+     * \brief Asks the workflow to clear itself.
+     */
     void clear();
+    /**
+     * \brief Change the zoom level for all widgets of the timeline.
+     * \param factor The zoom factor.
+     */
     void changeZoom( int factor );
+    /**
+     * \brief Change the duration of the project.
+     * \param duration Duration in frames.
+     */
     void setDuration( int duration );
+    /**
+     * \brief Change the currently selected tool.
+     */
     void setTool( ToolButtons button );
+    /**
+     * \brief Insert an item into the timeline.
+     * \param clip Clip to insert.
+     * \param track The track's number.
+     * \param start The position in frames.
+     * \param
+     */
     void actionAddClip( Clip* clip, unsigned int track, qint64 start, MainWorkflow::TrackType );
+    /**
+     * \brief Move an item in the timeline.
+     * \param uuid The Universally Unique Identifier of the item.
+     * \param track The new track of the item.
+     * \param time The new position (in frames) of the item.
+     */
     void actionMoveClip( const QUuid& uuid, unsigned int track, qint64 time, MainWorkflow::TrackType );
+    /**
+     * \brief Remove an item from the timeline.
+     * \param uuid The Universally Unique Identifier of the item.
+     * \param track The current track of the item.
+     */
     void actionRemoveClip( Clip* clip, unsigned int track, MainWorkflow::TrackType );
 
 protected:
