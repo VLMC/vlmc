0cf02d0dfb3a65f45b284c0026e6c0985c72b40c
Ludovic Fauvet <etix@l0cal.com>
2010-01-12 00:58:12 +0100

    Cross compiling for Windows works !

---
+++ cmake/toolchain-win32.cmake
@@ -0,0 +1,17 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME Windows)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   /usr/bin/i486-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER /usr/bin/i486-mingw32-g++)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  contribs)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
