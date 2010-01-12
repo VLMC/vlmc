d9f2c0e86b23cfca11a3174e3a725f5046054fbf
Ludovic Fauvet <etix@l0cal.com>
2010-01-12 01:19:31 +0100

    cmake: use absolute path to contribs

--- cmake/toolchain-win32.cmake
+++ cmake/toolchain-win32.cmake
@@ -8,7 +8,7 @@ SET(CMAKE_C_COMPILER   /usr/bin/i486-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER /usr/bin/i486-mingw32-g++)
 
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH  contribs)
+SET(CMAKE_FIND_ROOT_PATH  ${CMAKE_SOURCE_DIR}/contribs)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
