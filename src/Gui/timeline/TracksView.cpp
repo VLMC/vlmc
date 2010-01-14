d7b92588452e7795b34845a39748ab047fb40520
Ludovic Fauvet <etix@l0cal.com>
2010-01-14 16:12:57 +0100

    Timeline: fix an error when propagating old values to a linked item.
    Signed-off-by: Hugo Beauzee-Luyssen <beauze.h@gmail.com>

--- src/Gui/timeline/TracksView.cpp
+++ src/Gui/timeline/TracksView.cpp
@@ -699,15 +699,15 @@ void TracksView::mouseReleaseEvent( QMouseEvent* event )
         // Update the linked item too
         if ( m_actionItem->groupItem() )
         {
-            m_actionItem->groupItem()->oldTrackNumber = m_actionItem->groupItem()->trackNumber();
-            m_actionItem->groupItem()->oldPosition = m_actionItem->groupItem()->startPos();
-
             Commands::trigger( new Commands::MainWorkflow::MoveClip( m_mainWorkflow,
                                                                      m_actionItem->groupItem()->clip()->getUuid(),
                                                                      m_actionItem->groupItem()->oldTrackNumber,
                                                                      m_actionItem->groupItem()->trackNumber(),
                                                                      m_actionItem->groupItem()->startPos(),
                                                                      m_actionItem->groupItem()->mediaType() ) );
+
+            m_actionItem->groupItem()->oldTrackNumber = m_actionItem->groupItem()->trackNumber();
+            m_actionItem->groupItem()->oldPosition = m_actionItem->groupItem()->startPos();
         }
 
         m_actionItem->oldTrackNumber = m_actionItem->trackNumber();
