1093122f55cb9de7ac23932c4f4186661fb8002d
Christophe Courtaut <christophe.courtaut@gmail.com>
2010-01-21 23:51:57 +0100

    Do not use audio related option in image/video workflows

--- src/Workflow/VideoClipWorkflow.cpp
+++ src/Workflow/VideoClipWorkflow.cpp
@@ -58,7 +58,6 @@ VideoClipWorkflow::initVlcOutput()
     m_vlcMedia->setVideoLockCallback( reinterpret_cast<void*>( getLockCallback() ) );
     m_vlcMedia->setVideoUnlockCallback( reinterpret_cast<void*>( getUnlockCallback() ) );
     m_vlcMedia->addOption( ":sout-transcode-vcodec=RV24" );
-    m_vlcMedia->addOption( ":sout-transcode-acodec=fl32" );
     m_vlcMedia->addOption( ":sout-smem-time-sync" );
 
     sprintf( buffer, ":sout-transcode-width=%i",
